827f053a5b788dea989e54d0b6b108242c8fb044
Ashley Oz <ozdemirny@hotmail.com>
2020-04-27 14:34:03 -0400

    Day 29 class

---
+++ src/day29_Wrapper_ArrayList/WrapperClass_Practice.java
@@ -0,0 +1,43 @@
+package day29_Wrapper_ArrayList;
+
+import java.util.Arrays;
+
+/*
+Given;
+String[] arr={"1","2.5","3","3.5","4.5"}
+find thhe maximum and minimum numbers
+		NOTE: Do not use sort method
+ */
+public class WrapperClass_Practice {
+    public static void main(String[] args) {
+        double[] arr={1,2.5,3,3.5,4,4.5};
+        double max= Double.MIN_VALUE;
+        for (String each: arr){//5,each:1,2.5,3,3.5,4,4.5
+            double num= Double.valueOf(each);
+
+            if (num > max){
+                max=num;
+
+            }
+
+        }
+        System.out.println(max);
+        System.out.println("=========================");
+        String[] arr2= {"1","2.5","3","3.5","4.5"};
+
+        double[] numbers= new double[arr2.length]; //[0.0,0.0,0.0,0.0]
+
+        for (int i=0;i<arr2.length;i++){
+            numbers[i]= Double.parseDouble(arr2[i]);
+        }
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(numbers.length-1);
+
+
+
+
+
+
+    }
+}
